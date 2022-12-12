4f45e9a5880cd4c5a4471c9b79df60cbfe20cfe3
Michael Green <green@strato.de>
2022-12-12 12:52:01 +0100

    bump version number

--- lib/RPC/Switch/Client.pm
+++ lib/RPC/Switch/Client.pm
@@ -1,7 +1,7 @@
 package RPC::Switch::Client;
 use Mojo::Base 'Mojo::EventEmitter';
 
-our $VERSION = '0.21'; # VERSION
+our $VERSION = '0.22'; # VERSION
 
 #
 # Mojo's default reactor uses EV, and EV does not play nice with signals
